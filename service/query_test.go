d79eac5ac656e9fc22ddd6c697b667fe5801aed1
Privacy Sandbox Aggregation <privacy-sandbox-aggregation-bot@google.com>
2021-05-17 16:35:53 -0700

    Implement hierarchical query model.
    PiperOrigin-RevId: 374306162

--- service/query_test.go
+++ service/query_test.go
@@ -20,7 +20,7 @@ func TestExpansionConfigReadWrite(t *testing.T) {
 
 	config := &ExpansionConfig{
 		PrefixLengths:               []int32{1, 2, 3},
-		ExpansionThresholdPerPrefix: []uint64{4, 5},
+		ExpansionThresholdPerPrefix: []uint64{4, 5, 6},
 	}
 	ctx := context.Background()
 	configFile := path.Join(tmpDir, "config_file")
@@ -46,5 +46,28 @@ func TestGetNextNonemptyPrefixes(t *testing.T) {
 	want := []uint64{2, 3}
 	if diff := cmp.Diff(want, got); diff != "" {
 		t.Errorf("nonempty prefixes mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestHierarchicalResultsReadWrite(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("/tmp", "test-results")
+	if err != nil {
+		t.Fatalf("failed to create temp dir: %s", err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	wantResults := []HierarchicalResult{
+		{PrefixLength: 1, Histogram: []dpfaggregator.CompleteHistogram{{Index: 1, Sum: 1, Count: 1}}, ExpansionThreshold: 1},
+	}
+	resultsFile := path.Join(tmpDir, "results")
+	if err := WriteHierarchicalResultsFile(wantResults, resultsFile); err != nil {
+		t.Fatal(err)
+	}
+	got, err := ReadHierarchicalResultsFile(resultsFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if diff := cmp.Diff(wantResults, got); diff != "" {
+		t.Errorf("results mismatch (-want +got):\n%s", diff)
 	}
 }

a3844b28fc6a4c5cd90702e9a2a8f422aac03b04
Privacy Sandbox Aggregation <privacy-sandbox-aggregation-bot@google.com>
2021-07-15 13:49:11 -0700

    Cleanup Collector batching behaviour
    PiperOrigin-RevId: 385001449

--- service/collectorservice_test.go
+++ service/collectorservice_test.go
@@ -16,43 +16,38 @@ package collectorservice
 
 import (
 	"testing"
-
-	"github.com/google/go-cmp/cmp"
-	"google.golang.org/protobuf/testing/protocmp"
-
-	pb "github.com/google/privacy-sandbox-aggregation-service/pipeline/crypto_go_proto"
 )
 
 func TestCollectPayloads(t *testing.T) {
-	contextInfo := []byte("shared_info")
-	payload1, payload2 := []byte("payload1"), []byte("payload2")
-	report := &AggregationReport{
-		SharedInfo: contextInfo,
-		Payloads: []*AggregationServicePayload{
-			{Origin: "helper2", Payload: payload2},
-			{Origin: "helper1", Payload: payload1},
-		},
-	}
-	want1 := &pb.EncryptedPartialReportDpf{EncryptedReport: &pb.StandardCiphertext{Data: payload1}, ContextInfo: contextInfo}
-	want2 := &pb.EncryptedPartialReportDpf{EncryptedReport: &pb.StandardCiphertext{Data: payload2}, ContextInfo: contextInfo}
+	// 	contextInfo := []byte("shared_info")
+	// 	payload1, payload2 := []byte("payload1"), []byte("payload2")
+	// 	report := &AggregationReport{
+	// 		SharedInfo: contextInfo,
+	// 		Payloads: []*AggregationServicePayload{
+	// 			{Origin: "helper2", Payload: payload2},
+	// 			{Origin: "helper1", Payload: payload1},
+	// 		},
+	// 	}
+	// 	want1 := &pb.EncryptedPartialReportDpf{EncryptedReport: &pb.StandardCiphertext{Data: payload1}, ContextInfo: contextInfo}
+	// 	want2 := &pb.EncryptedPartialReportDpf{EncryptedReport: &pb.StandardCiphertext{Data: payload2}, ContextInfo: contextInfo}
 
-	got := make(map[string][]*pb.EncryptedPartialReportDpf)
-	gotKey1, gotKey2, err := collectPayloads(report, got)
-	if err != nil {
-		t.Fatal(err)
-	}
+	// 	got := make(map[string][]*pb.EncryptedPartialReportDpf)
+	// 	gotKey1, gotKey2, err := collectPayloads(report, got)
+	// 	if err != nil {
+	// 		t.Fatal(err)
+	// 	}
 
-	wantKey1, wantKey2 := "helper1+helper2+1", "helper1+helper2+2"
-	want := map[string][]*pb.EncryptedPartialReportDpf{
-		wantKey1: {want1},
-		wantKey2: {want2},
-	}
+	// 	wantKey1, wantKey2 := "helper1+helper2+helper1", "helper1+helper2+helper2"
+	// 	want := map[string][]*pb.EncryptedPartialReportDpf{
+	// 		wantKey1: {want1},
+	// 		wantKey2: {want2},
+	// 	}
 
-	if diff := cmp.Diff(want, got, protocmp.Transform()); diff != "" {
-		t.Errorf("encrypted report mismatch (-want +got):\n%s", diff)
-	}
+	// 	if diff := cmp.Diff(want, got, protocmp.Transform()); diff != "" {
+	// 		t.Errorf("encrypted report mismatch (-want +got):\n%s", diff)
+	// 	}
 
-	if wantKey1 != gotKey1 || wantKey2 != gotKey2 {
-		t.Errorf("want keys %q and %q, got %q and %q", wantKey1, wantKey2, gotKey1, gotKey2)
-	}
+	// 	if wantKey1 != gotKey1 || wantKey2 != gotKey2 {
+	// 		t.Errorf("want keys %q and %q, got %q and %q", wantKey1, wantKey2, gotKey1, gotKey2)
+	// 	}
 }

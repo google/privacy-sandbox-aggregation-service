6dbfead0de8c9229b25043951ae964e32ed01ef8
Privacy Sandbox Aggregation <privacy-sandbox-aggregation-bot@google.com>
2021-12-14 08:59:04 -0800

    Rename the query tool binary.
    PiperOrigin-RevId: 416316487

--- service/aggregation_query_tool.go
+++ service/aggregation_query_tool.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// This binary queries the hierarchical aggregation results by publishing the requests on certain PubSub topics.
+// This binary queries the aggregation results by publishing the requests on certain PubSub topics.
 package main
 
 import (
